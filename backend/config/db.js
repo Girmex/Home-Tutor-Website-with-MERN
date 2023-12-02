9d9c8fc7087042acd9fca4c605b3d2e88ea1da60
Girma Moges <girmacoeng@gmail.com>
2023-12-02 13:18:15 +0300

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,11 @@
+//Connection file to mongo db
+import mongoose from "mongoose";
+const connectDB = async () => {
+  try {const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit();
+  }
+};
+export default connectDB;
